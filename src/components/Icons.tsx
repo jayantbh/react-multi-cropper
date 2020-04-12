02e311158b76c40f8f2ad2590209e92582db3c02
Jayant Bhawal <bhawal.jayant@gmail.com>
2020-04-12 16:26:06 +0530

    Fix types for Icons.tsx.

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,12 +1,16 @@
-import React, { FC } from 'react';
+import React, { FC, HTMLAttributes } from 'react';
 import css from './Icons.module.scss';
 
-export const DeleteIcon: FC<any> = (props) => (
+export const DeleteIcon: FC<HTMLAttributes<HTMLDivElement>> = (props) => (
   <div className={css['icon-container']} {...props}>
     &times;
   </div>
 );
 
-export const NumberIcon: FC<{ number: number }> = ({ number }) => (
-  <div className={css.number}>{number}</div>
+export const NumberIcon: FC<
+  HTMLAttributes<HTMLDivElement> & { number: number }
+> = ({ number, ...props }) => (
+  <div {...props} className={`${css.number} ${props.className}`}>
+    {number}
+  </div>
 );
