ba42b8731f413a36fde569de963b0f15d9c37dbe
Jayant Bhawal <bhawal.jayant@gmail.com>
2020-04-14 19:00:38 +0530

    Add check for window in polyfills.js for SSR.

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,21 +1,23 @@
 import { default as ROPolyfill } from 'resize-observer-polyfill';
 
-window.ResizeObserver = window.ResizeObserver || ROPolyfill;
+if (typeof window !== 'undefined') {
+  window.ResizeObserver = window.ResizeObserver || ROPolyfill;
 
-if (!window.OffscreenCanvas) {
-  window.OffscreenCanvas = class OffscreenCanvas {
-    constructor(width, height) {
-      this.canvas = document.createElement('canvas');
-      this.canvas.width = width;
-      this.canvas.height = height;
+  if (!window.OffscreenCanvas) {
+    window.OffscreenCanvas = class OffscreenCanvas {
+      constructor(width, height) {
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = width;
+        this.canvas.height = height;
 
-      this.canvas.convertToBlob = () => {
-        return new Promise((resolve) => {
-          this.canvas.toBlob(resolve);
-        });
-      };
+        this.canvas.convertToBlob = () => {
+          return new Promise((resolve) => {
+            this.canvas.toBlob(resolve);
+          });
+        };
 
-      return this.canvas;
-    }
-  };
+        return this.canvas;
+      }
+    };
+  }
 }
